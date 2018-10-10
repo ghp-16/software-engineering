e104e0741ea33a529ae69566c4acd074156966a6
叶翀 <2016011387@secoder.net>
2018-10-10 22:56:28 +0800

    merged

---
+++ static/main.js
@@ -0,0 +1,18 @@
+var myHeading = document.getElementById('type');
+myHeading.textContent = '目前身份：学生';
+function type1()
+{
+    myHeading.textContent = '目前身份：学生';
+}
+function type2()
+{
+    myHeading.textContent = '目前身份：评委';
+}
+function type3()
+{
+    myHeading.textContent = '目前身份：队伍管理员';
+}
+function type4()
+{
+    myHeading.textContent = '目前身份：平台管理员';
+}
