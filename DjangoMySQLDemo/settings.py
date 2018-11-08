8ee10c283c644429d0ccea8102d708099326bdb6
秦笑容 <2016011337@secoder.net>
2018-11-08 12:07:42 +0800

    Feat #71 by improving check-in function.

--- DjangoMySQLDemo/settings.py
+++ DjangoMySQLDemo/settings.py
@@ -125,3 +125,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(os.path.dirname(__file__), '../static/').replace('\\', '/'),)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
